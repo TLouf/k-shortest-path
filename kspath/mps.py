0e0f493f445f1d11359de268b29de600175261cd
TLouf <loufthomas@gmail.com>
2025-01-16 11:16:40 +0100

    fmt

--- kspath/mps.py
+++ kspath/mps.py
@@ -2,7 +2,7 @@
 Martins, Pascoal and Santos deviation path algorithm.
 """
 
-from heapq import heappush, heappop
+from heapq import heappop, heappush
 from itertools import count
 
 import networkx as nx
@@ -10,8 +10,8 @@ import networkx as nx
 
 # Modified from networkx/algorithms/simple_paths.py:
 class PathBuffer(object):
-    """Heap priority queue to add and remove paths in sorted order
-    """
+    """Heap priority queue to add and remove paths in sorted order"""
+
     def __init__(self):
         self._paths = set()
         self._sorted_paths = list()
@@ -23,36 +23,35 @@ class PathBuffer(object):
     def push(self, cost, path, deviation_index, deviation_path_cost):
         """Adds relevant info for a path to the priority queue.
 
-            Parameters
-            ----------
-                cost : float
-                    Cost of the path
+        Parameters
+        ----------
+            cost : float
+                Cost of the path
 
-                path : list[str | int]
-                    Nodes of the path
+            path : list[str | int]
+                Nodes of the path
 
-                deviation_index : int
+            deviation_index : int
 
-                deviation_path_cost : float
+            deviation_path_cost : float
         """
         hashable_path = tuple(path)
         if hashable_path not in self._paths:
-            heappush(self._sorted_paths, (cost,
-                                          next(self._counter),
-                                          path,
-                                          deviation_index,
-                                          deviation_path_cost))
+            heappush(
+                self._sorted_paths,
+                (cost, next(self._counter), path, deviation_index, deviation_path_cost),
+            )
             self._paths.add(hashable_path)
 
     def pop(self):
         """Returns relevant info for a path from the priority queue.
 
-            Returns
-            -------
-                (cost,
-                 path,
-                 deviation_index,
-                 deviation_path) : tuple[float, tuple[str | int], int, float]
+        Returns
+        -------
+            (cost,
+             path,
+             deviation_index,
+             deviation_path) : tuple[float, tuple[str | int], int, float]
         """
         (cost, _, path, deviation_index, deviation_path_cost) = heappop(
             self._sorted_paths
@@ -74,12 +73,10 @@ class SingleTargetDeviationPathAlgorithm(object):
     paths are searched without finding a simple path. This modification seems
     to run slightly faster.
     """
-    def __init__(self,
-                 G,
-                 G_reverse,
-                 target,
-                 weight='weight',
-                 max_consecutive_cycles=500):
+
+    def __init__(
+        self, G, G_reverse, target, weight="weight", max_consecutive_cycles=500
+    ):
         """Input Parameters
 
         Parameters
@@ -104,7 +101,7 @@ class SingleTargetDeviationPathAlgorithm(object):
                 deviation paths.
         """
         if target not in G:
-            raise nx.NodeNotFound('target node %s not in graph' % target)
+            raise nx.NodeNotFound("target node %s not in graph" % target)
 
         dist, paths = nx.single_source_dijkstra(G_reverse, target)
         for node in paths:
@@ -120,11 +117,7 @@ class SingleTargetDeviationPathAlgorithm(object):
         self._weight = weight
 
     @classmethod
-    def create_from_graph(cls,
-                          G,
-                          target,
-                          weight='weight',
-                          max_consecutive_cycles=500):
+    def create_from_graph(cls, G, target, weight="weight", max_consecutive_cycles=500):
         """Creates graph and graph_reverse from G with
         only `weight` attribute.
         """
@@ -138,10 +131,8 @@ class SingleTargetDeviationPathAlgorithm(object):
         graph_reverse = graph.reverse()
 
         if weight is None:
-            weight = 'weight'
-        return cls(
-            graph, graph_reverse, target, weight, max_consecutive_cycles
-        )
+            weight = "weight"
+        return cls(graph, graph_reverse, target, weight, max_consecutive_cycles)
 
     def _update_sorted_arcs(self, tail_node):
         """Updates _sorted_arcs dict."""
@@ -154,7 +145,7 @@ class SingleTargetDeviationPathAlgorithm(object):
                 cost = (
                     self._dist[head_node]
                     - tail_node_to_target_dist
-                    + self.graph[tail_node][head_node]['weight']
+                    + self.graph[tail_node][head_node]["weight"]
                 )
                 cost_head_node_list.append((cost, head_node))
         cost_head_node_list.sort()
@@ -172,23 +163,20 @@ class SingleTargetDeviationPathAlgorithm(object):
                 head_node_to_index[head_node] = index
 
         self._sorted_arcs[tail_node] = {
-            'cost_head_node_list': cost_head_node_list,
-            'head_node_to_index': head_node_to_index
+            "cost_head_node_list": cost_head_node_list,
+            "head_node_to_index": head_node_to_index,
         }
 
-    def mps_deviation_paths(self,
-                            path_cost,
-                            path,
-                            deviation_index,
-                            deviation_path_cost,
-                            list_x):
+    def mps_deviation_paths(
+        self, path_cost, path, deviation_index, deviation_path_cost, list_x
+    ):
         """Implementation for Martins, Pascoal and Santos (MPS) deviation
         path algorithm.
         """
         for i in range(deviation_index, len(path) - 1):
             v_i = path[i]
             v_j = path[i + 1]
-            root_path = path[:i + 1]
+            root_path = path[: i + 1]
             root_path_nodes = set(root_path)
 
             # add a simple check to stop search for deviation paths
@@ -208,18 +196,17 @@ class SingleTargetDeviationPathAlgorithm(object):
             if v_i not in self._sorted_arcs:
                 self._update_sorted_arcs(v_i)
 
-            vj_index = self._sorted_arcs[v_i]['head_node_to_index'][v_j]
-            cost_head_node_list = (
-                self._sorted_arcs[v_i]['cost_head_node_list'][vj_index + 1:]
-            )
+            vj_index = self._sorted_arcs[v_i]["head_node_to_index"][v_j]
+            cost_head_node_list = self._sorted_arcs[v_i]["cost_head_node_list"][
+                vj_index + 1 :
+            ]
             for cost, head_node in cost_head_node_list:
                 if head_node not in root_path_nodes:
                     new_path = root_path + self._paths[head_node]
                     if i == deviation_index:
-                        list_x.push(deviation_path_cost + cost,
-                                    new_path,
-                                    i,
-                                    deviation_path_cost)
+                        list_x.push(
+                            deviation_path_cost + cost, new_path, i, deviation_path_cost
+                        )
                     else:
                         list_x.push(path_cost + cost, new_path, i, path_cost)
                     break
@@ -243,10 +230,12 @@ class SingleTargetDeviationPathAlgorithm(object):
             candidate_paths = PathBuffer()
 
             # first candidate path is the shortest path
-            candidate_paths.push(cost=0.0,
-                                 path=self._paths[source],
-                                 deviation_index=0,
-                                 deviation_path_cost=0.0)
+            candidate_paths.push(
+                cost=0.0,
+                path=self._paths[source],
+                deviation_index=0,
+                deviation_path_cost=0.0,
+            )
 
             consecutive_cycles = 0
             simple_paths_found = set()
@@ -276,17 +265,18 @@ class SingleTargetDeviationPathAlgorithm(object):
                     else:
                         consecutive_cycles += 1
 
-                    self.mps_deviation_paths(path_cost,
-                                             path,
-                                             deviation_index,
-                                             deviation_path_cost,
-                                             candidate_paths)
+                    self.mps_deviation_paths(
+                        path_cost,
+                        path,
+                        deviation_index,
+                        deviation_path_cost,
+                        candidate_paths,
+                    )
 
             if max_consecutive_cycles_reached:
-                for path in nx.shortest_simple_paths(self.graph,
-                                                     source,
-                                                     self.target,
-                                                     'weight'):
+                for path in nx.shortest_simple_paths(
+                    self.graph, source, self.target, "weight"
+                ):
                     if tuple(path) not in simple_paths_found:
                         yield path
 
@@ -308,6 +298,6 @@ class SingleTargetDeviationPathAlgorithm(object):
             networkx.NodeNotFound : If source is not in graph
         """
         if source not in self.graph:
-            raise nx.NodeNotFound('source node %s not in graph' % source)
+            raise nx.NodeNotFound("source node %s not in graph" % source)
 
         return self._shortest_simple_paths(source)
