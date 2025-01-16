d497306c4c8400638138983154433abc658aabc5
TLouf <loufthomas@gmail.com>
2025-01-16 11:46:34 +0100

    remove test deps from required ones

--- setup.py
+++ setup.py
@@ -8,19 +8,21 @@ You can install k-shortest-path with
 pipenv install
 pip install .
 """
-from setuptools import setup, find_packages
 
-setup(name='kspath',
-      version='0.1.1',
-      description='Implements algorithms for the K shortest path problem.',
-      author='Chong Hon Fah',
-      author_email='hfchong@data.gov.sg',
-      license='Apache License 2.0',
-      packages=find_packages(exclude=['tests']),
-      install_requires=["networkx>=2.1",
-                        "numpy>=1.13.3",
-                        "pandas>=0.21.0",
-                        "scikit-learn>=0.19.1"],
-      url='https://github.com/datagovsg/k-shortest-path',
-      classifiers=['Programming Language :: Python :: 3'],
-      zip_safe=False)
+from setuptools import find_packages, setup
+
+setup(
+    name="kspath",
+    version="0.1.1",
+    description="Implements algorithms for the K shortest path problem.",
+    author="Chong Hon Fah",
+    author_email="hfchong@data.gov.sg",
+    license="Apache License 2.0",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "networkx>=2.1",
+    ],
+    url="https://github.com/datagovsg/k-shortest-path",
+    classifiers=["Programming Language :: Python :: 3"],
+    zip_safe=False,
+)
